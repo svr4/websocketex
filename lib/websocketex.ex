658788bcebb2d3df6da9286b09f52b7e25c28dd5
Marcel Rivera <marcel.rivera@protonmail.com>
2017-09-07 00:26:02 -0400

    Added main methdod to test gen_tcp wrapper. Main method now listens for http requests.

--- lib/websocketex.ex
+++ lib/websocketex.ex
@@ -4,20 +4,43 @@ defmodule Websocketex do
   """
 
 	def main do
-		{:ok, listenSocket} = listen(5678)
+		# Listen socket, in this case
+		{:ok, listenSocket} = listen(5678, [:binary, {:packet, :http_bin}, {:active, false}])
 		{:ok, socket} = accept(listenSocket)
-		case recv(socket, 0) do
-			{:ok, packet} -> IO.puts packet
-			{:error, reason} -> IO.puts reason
-		end
+		#{:ok, {:http_request, :GET, {:abs_path, "/"}, {1, 1}}}
+		recv(socket, 0)
+		#case recv(socket, 0) do
+			#{:ok, packet} -> IO.puts packet
+			#{:error, reason} -> IO.puts reason
+			#_ -> IO.puts "WTF"
+		#end
+		:gen_tcp.send(socket, "HTTP/1.1 200 OK \r\n")
+		close(socket)
+		
 	end
 
+	'''
+	def server_loop({socket}) do	
+		{:ok, socket} = accept(socket)
+		receive_loop(socket, [])
+		close(socket)
+		server_loop(socket)
+	end
+
+	def receive_loop(socket, bs \\ []) do
+		case recv(socket, 0) do
+			{:ok, packet} -> receive_loop(socket, [bs, packet])
+			{:error, reason} -> {:error, reason}
+		end
+	end
+	'''
 	def close(socket) do
-		shutdown(socket, :read_write)
+		#shutdown(socket, :read_write)
 		:gen_tcp.close(socket)
 	end
 
-	def listen(port, options \\ [:binary, {:packet, 0}, {:active, false}]) do
+	#[:binary, {:packet, 0}, {:active, false}]
+	def listen(port, options \\ []) do
 		case :gen_tcp.listen(port, options) do
 			{:ok, listenSocket} -> {:ok, listenSocket}
 			{:error, reason} -> 
@@ -41,6 +64,7 @@ defmodule Websocketex do
 
 	def shutdown(socket, how) do
 		case :gen_tcp.shutdown(socket, how) do
+			:ok -> :ok
 			{:error, reason} ->
 				raise reason
 		end
@@ -57,7 +81,7 @@ defmodule Websocketex do
 
 	def connect(address, port, options, timeout) do
 		case :gen_tcp.connect(address, port, options, timeout) do
-			{:ok, socket} ->
+			{socket} ->
 				{socket}
 			{:error, reason} ->
 				raise reason
@@ -71,7 +95,7 @@ defmodule Websocketex do
 	def recv(socket, length, timeout) do
 		case :gen_tcp.recv(socket, length, timeout) do
 			{:ok, packet} -> 
-				{packet}
+				{:ok, packet}
 
 			{:error, reason} ->
 				raise reason
