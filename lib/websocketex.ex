ad9b4b19c80a20316935b6794e8f1b8bc737a5b6
Marcel Rivera <marcel.rivera@protonmail.com>
2017-09-09 12:38:04 -0400

    Working with http packets in tcp sockets. Reading http request line by line in order to extract headers and other information

--- lib/websocketex.ex
+++ lib/websocketex.ex
@@ -4,88 +4,52 @@ defmodule Websocketex do
   """
 
 	def main do
-		# Listen socket, in this case
-		{:ok, listenSocket} = listen(5678, [:binary, {:packet, :http_bin}, {:active, false}])
+		# Listen socket, in this case	
+		#[:binary, {:packet, :http_bin}, {:active, false}]
+		{:ok, listenSocket} = listen(5678)
 		{:ok, socket} = accept(listenSocket)
 		#{:ok, {:http_request, :GET, {:abs_path, "/"}, {1, 1}}}
-		recv(socket, 0)
 		#case recv(socket, 0) do
-			#{:ok, packet} -> IO.puts packet
-			#{:error, reason} -> IO.puts reason
+			#{:ok, packet} -> packet
+			#{:error, reason} -> reason
 			#_ -> IO.puts "WTF"
 		#end
-		:gen_tcp.send(socket, "HTTP/1.1 200 OK \r\n")
-		close(socket)
-		
-	end
-
-	'''
-	def server_loop({socket}) do	
-		{:ok, socket} = accept(socket)
-		receive_loop(socket, [])
-		close(socket)
-		server_loop(socket)
-	end
-
-	def receive_loop(socket, bs \\ []) do
-		case recv(socket, 0) do
-			{:ok, packet} -> receive_loop(socket, [bs, packet])
-			{:error, reason} -> {:error, reason}
-		end
-	end
-	'''
-	def close(socket) do
+		#{:ok, httpPacket} = recv(socket, 0)
+		recv(socket, 0)
+		recv(socket, 0)
+		#:gen_tcp.send(socket, "HTTP/1.1 200 OK \r\n")
 		#shutdown(socket, :read_write)
+		#close(socket)
+		#{http_request, http_method, http_uri, http_version} = httpPacket
+	end
+
+	def close(socket) do
 		:gen_tcp.close(socket)
 	end
 
 	#[:binary, {:packet, 0}, {:active, false}]
-	def listen(port, options \\ []) do
-		case :gen_tcp.listen(port, options) do
-			{:ok, listenSocket} -> {:ok, listenSocket}
-			{:error, reason} -> 
-				raise reason
-		end
+	def listen(port, options \\ [:list, {:packet, :http_bin}, {:active, false}, {:reuseaddr, true}]) do
+		:gen_tcp.listen(port, options)
 	end
 
 	def accept(socket) do
-		case :gen_tcp.accept(socket) do
-			{:ok, socket} -> {:ok, socket}
-			{:error, reason} -> raise reason
-		end
+		:gen_tcp.accept(socket)
 	end
 
 	def accept(socket, timeout) do
-		case :gen_tcp.accept(socket, timeout) do
-			{:ok, socket} -> {:ok, socket}
-			{:error, reason} ->	raise reason
-		end
+		:gen_tcp.accept(socket, timeout)
 	end
 
 	def shutdown(socket, how) do
-		case :gen_tcp.shutdown(socket, how) do
-			:ok -> :ok
-			{:error, reason} ->
-				raise reason
-		end
+		:gen_tcp.shutdown(socket, how)
 	end
 
 	def connect(address, port, options) do
-		case :gen_tcp.connect(address, port, options) do
-			{:ok, socket} ->
-				{socket}
-			{:error, reason} ->
-				raise reason
-		end
+		:gen_tcp.connect(address, port, options)
 	end
 
 	def connect(address, port, options, timeout) do
-		case :gen_tcp.connect(address, port, options, timeout) do
-			{socket} ->
-				{socket}
-			{:error, reason} ->
-				raise reason
-		end
+		:gen_tcp.connect(address, port, options, timeout)
 	end
 
 	def recv(socket, length) do
@@ -93,27 +57,15 @@ defmodule Websocketex do
 	end
 
 	def recv(socket, length, timeout) do
-		case :gen_tcp.recv(socket, length, timeout) do
-			{:ok, packet} -> 
-				{:ok, packet}
-
-			{:error, reason} ->
-				raise reason
-		end 
+		:gen_tcp.recv(socket, length, timeout)
 	end
 
 	def send(socket, packet) do
-		case :gen_tcp.send(socket, packet) do
-			{:error, reason} ->
-				raise reason
-		end
+		:gen_tcp.send(socket, packet)
 	end
 
 	def controlling_process(socket, pid) do
-		case :gen_tcp.controlling_process(socket, pid) do
-			{:error, reason} ->
-				raise reason
-		end
+		:gen_tcp.controlling_process(socket, pid)
 	end
 
 end
